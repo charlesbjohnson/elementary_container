7d4d939b860ac919dbf6cdc4a75084653090e7aa
Charles B Johnson <mail@cbjohnson.info>
2015-09-10 00:20:33 -0700

    Create main for server. Create IoC-ish bootstrapper to start the server and configure its plugins.

---
+++ src/github.com/charlesbjohnson/elementary_container/cmd/headmaster/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/charlesbjohnson/elementary_container/headmaster/bootstrap"
+
+func main() {
+	bootstrap.Run()
+}
---
+++ src/github.com/charlesbjohnson/elementary_container/headmaster/bootstrap/bootstrap.go
@@ -0,0 +1,25 @@
+package bootstrap
+
+import (
+	"os"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/charlesbjohnson/elementary_container/headmaster"
+	"github.com/joho/godotenv"
+)
+
+func Run() {
+	logger := logrus.New()
+	logger.Out = os.Stdout
+
+	if err := godotenv.Load(); err != nil {
+		logger.WithField("file", ".env").Fatal(err)
+	}
+
+	server, err := headmaster.New(logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	server.Run()
+}
